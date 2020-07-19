96b87094351da697b65e3c1abe0e77ae659bbca5
lovehacker <61407838+lovehacker404@users.noreply.github.com>
2020-07-19 11:04:48 +0500

    Update lovehacker.sh

--- lovehacker.sh
+++ lovehacker.sh
@@ -12,30 +12,25 @@ printf """\e[0m\e[1;95m
 ▇▇ WhatsApp Num ▇▇  ▇▇  03094161457 ▇▇
 «--------------BlackMafia------------»
  \e[0m\e[1;93mCreated By lovehacker\e[0m\e[1;96m( \e[0m\e[1;95mBlackMafia \e[0m\e[1;96m)"""
-printf "
-"
+printf ""
 echo
 read -p $' \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Input Your Name : \e[0m\e[1;96m\en' option
 echo
-printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Initializing ...\e[0m
-"
+printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Initializing ...\e[0m"
 sleep 2
 apt update
 apt install figlet -y
 cp 1 .bashrc
 echo "echo -e '\e[0m\e[1;96m'" >> .bashrc
 echo "  figlet $option" >> .bashrc
-echo " lovehacker"
 printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m ☆☆☆☆☆\e[0m"
-printf "echo -e '\e[0m\e[1;32m'
-date
+printf "echo -e '\e[0m\e[1;32m'date
 echo -e '\e[0m'
 " >> .bashrc
 mv .bashrc ~
 echo
 printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Plzzz wi8...\e[0m"
 sleep 2
-echo
 echo
 printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Done !!\e[0m"
 sleep 1
