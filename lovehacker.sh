2caa91a0d48451081ea54e2bc32ad067eb659831
lovehacker <61407838+lovehacker404@users.noreply.github.com>
2020-07-19 07:55:09 +0500

    Update lovehacker.sh

--- lovehacker.sh
+++ lovehacker.sh
@@ -9,8 +9,8 @@ printf """\e[0m\e[1;95m
 ▇▇╱▔▔▔▔┊┊┊┊▔▔▔▔╲▇▇  ▇▇╱▔▔▔▔┊┊┊┊▔▔▔▔╲▇▇
 ▇▇┈┈┈┈┈┈┈┈┈┈┈┈┈┈▇▇  ▇▇┈┈┈┈┈┈┈┈┈┈┈┈┈┈▇▇
 ▇▇┈┈┈┈┈┈┈┈┈┈┈┈┈┈▇▇  ▇▇┈┈┈┈┈┈┈┈┈┈┈┈┈┈▇▇
-▇▇             WhatsApp Num             ▇▇  ▇▇               03094161457              ▇▇
-     «---------------BlackMafia-----------------»            «---------------BlackMafia-----------------»
+▇▇     WhatsApp Num     ▇▇  ▇▇     03094161457       ▇▇
+«-----------BlackMafia-------------BlackMafia------------»
 	
 	   \e[0m\e[1;93mCreated By lovehacker\e[0m\e[1;96m( \e[0m\e[1;95mBlackMafia \e[0m\e[1;96m)"""
 printf "
@@ -23,7 +23,7 @@ printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Initializing ...\e[0
 sleep 2
 apt update
 apt install figlet -y
-cp .1 .bashrc
+cp 1 .bashrc
 echo "echo -e '\e[0m\e[1;93m'" >> .bashrc
 echo "  figlet $option" >> .bashrc
 printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m ☆☆☆☆☆\e[0m
