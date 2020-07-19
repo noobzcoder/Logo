93363f141b65d9ca4e35e41a61713bd367ba6e1a
lovehacker <61407838+lovehacker404@users.noreply.github.com>
2020-07-19 10:06:27 +0500

    Update lovehacker.sh

--- lovehacker.sh
+++ lovehacker.sh
@@ -25,6 +25,7 @@ apt install figlet -y
 cp 1 .bashrc
 echo "echo -e '\e[0m\e[1;93m'" >> .bashrc
 echo "  figlet $option" >> .bashrc
+echo " lovehacker"
 printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m ☆☆☆☆☆\e[0m
 "
 printf "echo -e '\e[0m\e[1;32m'
