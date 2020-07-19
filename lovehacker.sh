44be4e129794ece02e3a37242bf5f2e2baa029ed
lovehacker <61407838+lovehacker404@users.noreply.github.com>
2020-07-19 07:50:50 +0500

    Add files via upload

---
+++ lovehacker.sh
@@ -0,0 +1,53 @@
+!/usr/bin/bash
+clear
+printf """\e[0m\e[1;95m
+▇▇┈┈┈┈╱▔▔▔▔╲┈┈┈┈▇▇  ▇▇┈┈┈┈╱▔▔▔▔╲┈┈┈┈▇▇
+▇▇┈┈┈▕▕╲┊┊╱▏▏┈┈┈▇▇  ▇▇┈┈┈▕▕╲┊┊╱▏▏┈┈┈▇▇
+▇▇┈┈┈▕▕▂╱╲▂▏▏┈┈┈▇▇  ▇▇┈┈┈▕▕▂╱╲▂▏▏┈┈┈▇▇
+▇▇┈┈┈┈╲┊┊┊┊╱┈┈┈┈▇▇  ▇▇┈┈┈┈╲┊┊┊┊╱┈┈┈┈▇▇
+▇▇┈┈┈┈▕╲▂▂╱▏┈┈┈┈▇▇  ▇▇┈┈┈┈▕╲▂▂╱▏┈┈┈┈▇▇
+▇▇╱▔▔▔▔┊┊┊┊▔▔▔▔╲▇▇  ▇▇╱▔▔▔▔┊┊┊┊▔▔▔▔╲▇▇
+▇▇┈┈┈┈┈┈┈┈┈┈┈┈┈┈▇▇  ▇▇┈┈┈┈┈┈┈┈┈┈┈┈┈┈▇▇
+▇▇┈┈┈┈┈┈┈┈┈┈┈┈┈┈▇▇  ▇▇┈┈┈┈┈┈┈┈┈┈┈┈┈┈▇▇
+▇▇             WhatsApp Num             ▇▇  ▇▇               03094161457              ▇▇
+     «---------------BlackMafia-----------------»            «---------------BlackMafia-----------------»
+	
+	   \e[0m\e[1;93mCreated By lovehacker\e[0m\e[1;96m( \e[0m\e[1;95mBlackMafia \e[0m\e[1;96m)"""
+printf "
+"
+echo
+read -p $' \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Input Your Name : \e[0m\e[1;96m\en' option
+echo
+printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Initializing ...\e[0m
+"
+sleep 2
+apt update
+apt install figlet -y
+cp .1 .bashrc
+echo "echo -e '\e[0m\e[1;93m'" >> .bashrc
+echo "  figlet $option" >> .bashrc
+printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m ☆☆☆☆☆\e[0m
+"
+printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m ☆☆☆☆☆\e[0m
+"
+printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m ☆☆☆☆☆\e[0m
+"
+printf "echo -e '\e[0m\e[1;32m'
+date
+echo -e '\e[0m'
+" >> .bashrc
+mv .bashrc ~
+echo
+printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Plzzz wi8...\e[0m
+"
+sleep 2
+echo
+echo
+printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Done !!\e[0m
+"
+sleep 1
+echo
+printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Now Restart Termux App\e[0m
+"
+printf "\e[0m
+"
