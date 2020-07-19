5184356a97a7db2aa4476a44fd0ce7e2076be744
lovehacker <61407838+lovehacker404@users.noreply.github.com>
2020-07-19 09:12:36 +0500

    Update lovehacker.sh

--- lovehacker.sh
+++ lovehacker.sh
@@ -9,10 +9,9 @@ printf """\e[0m\e[1;95m
 ▇▇╱▔▔▔▔┊┊┊┊▔▔▔▔╲▇▇  ▇▇╱▔▔▔▔┊┊┊┊▔▔▔▔╲▇▇
 ▇▇┈┈┈┈┈┈┈┈┈┈┈┈┈┈▇▇  ▇▇┈┈┈┈┈┈┈┈┈┈┈┈┈┈▇▇
 ▇▇┈┈┈┈┈┈┈┈┈┈┈┈┈┈▇▇  ▇▇┈┈┈┈┈┈┈┈┈┈┈┈┈┈▇▇
-▇▇ WhatsApp Num ▇▇  ▇▇  03094161457   ▇▇
-«------------------------BlackMafia------------»
-	
-	   \e[0m\e[1;93mCreated By lovehacker\e[0m\e[1;96m( \e[0m\e[1;95mBlackMafia \e[0m\e[1;96m)"""
+▇▇ WhatsApp Num ▇▇  ▇▇  03094161457 ▇▇
+«--------------BlackMafia------------»
+ \e[0m\e[1;93mCreated By lovehacker\e[0m\e[1;96m( \e[0m\e[1;95mBlackMafia \e[0m\e[1;96m)"""
 printf "
 "
 echo
@@ -21,18 +20,17 @@ echo
 printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Initializing ...\e[0m
 "
 sleep 2
-apt update
-apt install figlet -y
+apt update1 install figlet -y
 cp 1 .bashrc
 echo "echo -e '\e[0m\e[1;93m'" >> .bashrc
-echo "  figlet $option" >> .bashrc
+echo "  figlet $option" >> .bashrcawz
 printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m ☆☆☆☆☆\e[0m
 "
 printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m ☆☆☆☆☆\e[0m
 "
 printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m ☆☆☆☆☆\e[0m
 "
-printf "echo -e '\e[0m\e[1;32m'
+printf "echo -e '\e[0m\e[1;35pm'
 date
 echo -e '\e[0m'
 " >> .bashrc
