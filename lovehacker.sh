d551aace74527ee65cbce8b9702b3418793f58d0
lovehacker <61407838+lovehacker404@users.noreply.github.com>
2020-09-08 22:29:48 +0500

    Update lovehacker.sh

--- lovehacker.sh
+++ lovehacker.sh
@@ -13,9 +13,7 @@ printf """\e[0m\e[1;95m
 «--------------BlackMafia------------»
  \e[0m\e[1;93mCreated By lovehacker\e[0m\e[1;96m( \e[0m\e[1;95mBlackMafia \e[0m\e[1;96m)"""
 printf ""
-echo
 read -p $' \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Input Your Name : \e[0m\e[1;96m\en' option
-echo
 printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Initializing ...\e[0m"
 sleep 2
 apt update
@@ -25,10 +23,8 @@ echo "echo -e '\e[0m\e[1;96m'" >> .bashrc
 echo "  figlet $option" >> .bashrc
 printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m ☆☆☆☆☆\e[0m"
 printf "echo -e '\e[0m\e[1;32m'◁━━━━━━━━━━━━━━━━━━━━◈✙◈━━━━━━━━━━━━━━━━━━━━▷
-echo -e '\e[0m'
 " >> .bashrc
 mv .bashrc ~
-echo
 printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Plzzz wi8...\e[0m"
 sleep 2
 echo
@@ -36,5 +32,3 @@ printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Done !!\e[0m"
 sleep 1
 echo
 printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Now Restart Termux App\e[0m"
-printf " \e[35m
-"
