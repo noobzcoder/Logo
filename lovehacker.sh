d4b4f14d2a5c68faaddda26debb088efc61ef036
lovehacker <61407838+lovehacker404@users.noreply.github.com>
2020-09-21 01:20:40 +0500

    Update lovehacker.sh

--- lovehacker.sh
+++ lovehacker.sh
@@ -15,11 +15,10 @@ printf """\e[0m\e[1;95m
 read -p $' \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Input Your Name : \e[0m\e[1;96m\en' option
 printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Initializing ...\e[0m"
 sleep 2
-apt update
-apt install figlet -y
+pkg install fish -y
+fish
 cp 1 .bashrc
 echo "echo -e '\e[0m\e[1;96m'" >> .bashrc
-echo "  figlet $option" >> .bashrc
 printf "echo -e '\e[0m\e[1;32m'◁━━━━━━━━━━━━━━━━━━━━◈✙◈━━━━━━━━━━━━━━━━━━━━▷" >> .bashrc
 mv .bashrc ~
 printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Plzzz wi8...\e[0m"
